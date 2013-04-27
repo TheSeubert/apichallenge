df61097d2b9658f2e315a7d1096c2d7d7377bfc8
Justin Seubert <dude051@gmail.com>
2013-04-26 23:10:31 -0500

    Changed challenge 11 headers

--- challenge11.py
+++ challenge11.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 #
-#    This will create two 512MB CentOS 6.3 servers and a Cloud
-#    Load Balancer with the two servers as active nodes ready for HTTP
-#    request on port 80 using ServiceNet to the servers.
+# Create an SSL terminated load balancer (Create self-signed certificate.)
+# Create a DNS record that should be pointed to the load balancer.
+# Create Three servers as nodes behind the LB.
+#      Each server should have a CBS volume attached to it. (Size and type are irrelevant.)
+#      All three servers should have a private Cloud Network shared between them.
+#      Login information to all three servers returned in a readable format as the result of the script, including connection information.
 
 import os
 import pyrax
